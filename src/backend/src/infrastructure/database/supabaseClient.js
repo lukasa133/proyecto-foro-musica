160c188629caa743da0f703b75a2eb24e88c644a
Louis-Du <Luis.a.d.f.beto@gmail.com>
2026-07-30 20:54:58 -0500

    chore: Configurar cliente Supabase

--- src/backend/src/infrastructure/database/supabaseClient.js
+++ src/backend/src/infrastructure/database/supabaseClient.js
@@ -1,6 +1,13 @@
+/*
+    Scrip creado para la administrar el cliente de supabase.
+*/
+
+// Se importa especificamente el método createClient de la librería supabase.
 const { createClient } = require("@supabase/supabase-js");
+// Librería que permitirá la consulta del archivo .env.
 require("dotenv").config();
 
+// El cliente solo consultará SUPABASE_URL y SUPABASE_ANON_KEY del .env. 
 const supabase = createClient(
     process.env.SUPABASE_URL, 
     process.env.SUPABASE_ANON_KEY
